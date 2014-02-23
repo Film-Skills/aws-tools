3b12990f3bbd3e22d31ef6bd373a1d887909b296
Martin Brennan <mbrennan@filmskills.com.au>
2014-02-24 09:21:01 +1000

    Created skeleton of presign method

--- AWSTools/S3.cs
+++ AWSTools/S3.cs
@@ -8,5 +8,17 @@ namespace AWSTools
 {
     public class S3
     {
+        /// <summary>
+        /// Takes a key and a bucket and converts the
+        /// path into a presigned url for access to restricted
+        /// S3 content.
+        /// </summary>
+        /// <param name="key">The path to the file in S3.</param>
+        /// <param name="bucket">The bucket that the file is in.</param>
+        /// <returns></returns>
+        public static string PreSignURL(string key, string bucket)
+        {
+            return string.Empty;
+        }
     }
 }
